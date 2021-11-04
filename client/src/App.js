8ec52c317d5216afbe33262411e16b893c66a79a
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro.local>
2021-11-03 20:26:57 -0400

    Frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { RegistrationForm } from './components/RegistrationForm';
 import { RegistrationDisplay } from "./components/RegistrationsDisplay";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <RegistrationForm />
-      <RegistrationDisplay />
-    </div>
+    <BrowserRouter>
+    <Switch>
+        <Route exact path="/" component={RegistrationForm} />
+        <Route exact path="/display" component={RegistrationDisplay} />
+    </Switch>
+    </BrowserRouter>
+
   );
 }
 
